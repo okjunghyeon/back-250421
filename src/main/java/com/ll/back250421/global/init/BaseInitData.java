6de0230ec2be574729308256fdf8a40803105a44
jeonghyeon-ohk <ok6737@naver.com>
2025-04-21 14:07:14 +0900

    redis

--- src/main/java/com/ll/back250421/global/init/BaseInitData.java
+++ src/main/java/com/ll/back250421/global/init/BaseInitData.java
@@ -1,8 +1,9 @@
-package com.ll.back250421.global.init;
+package com.ll.back250421.global.initData;
 
 import com.ll.back250421.domain.post.post.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -32,5 +33,15 @@ public class BaseInitData {
 
         postService.write("제목 1", "내용 1");
         postService.write("제목 2", "내용 2");
+
+        self.get1Plus1();
+        self.get1Plus1();
+        self.get1Plus1();
+    }
+
+    @Cacheable("get1Plus1")
+    public int get1Plus1() {
+        System.out.println("get1Plus1 run!");
+        return 1 + 1;
     }
 }
