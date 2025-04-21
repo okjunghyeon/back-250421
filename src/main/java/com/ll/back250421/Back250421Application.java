b6bbffdd22988fac0b8f081607dc8a48105f7d59
jeonghyeon-ohk <ok6737@naver.com>
2025-04-21 11:36:50 +0900

    init back

---
+++ src/main/java/com/ll/back250421/Back250421Application.java
@@ -0,0 +1,13 @@
+package com.ll.back250421;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back250421Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Back250421Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/back250421/Back250421ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.back250421;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Back250421ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
